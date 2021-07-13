ef6e9e96e100f7135f24d54713f17a4e185d79e5
Lior agami <agamilior25@gmail.com>
2021-07-13 18:53:38 -0400

    fixing dir and adding test

--- static/boggle.js
+++ static/boggle.js
@@ -1,97 +1,90 @@
 class BoggleGame {
-    /* make a new game at this DOM id */
-  
-    constructor(boardId, secs = 60) {
-      this.secs = secs; // game length
-      this.showTimer();
-  
-      this.score = 0;
-      this.words = new Set();
-      this.board = $("#" + boardId);
-  
-      // every 1000 msec, "tick"
-      this.timer = setInterval(this.tick.bind(this), 1000);
-  
-      $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
+
+    constructor(board, secs = 60) {
+        this.secs = secs
+        this.words = new Set();
+        this.board = $("#" + board);
+        this.score = 0;
+        this.showTimer();
+
+        this.timer = setInterval(this.clock.bind(this), 1000);
+
+        $(".add-word", this.board).on("submit", this.submit.bind(this));
     }
-  
-    /* show word in list of words */
-  
+
     showWord(word) {
-      $(".words", this.board).append($("<li>", { text: word }));
+        $(".words", this.board).append($("<li>", { text: word }));
     }
-  
-    /* show score in html */
-  
+
+    showMsg(msg, cls) {
+        $(".msg", this.board)
+            .text(msg)
+            .removeClass()
+            .addClass(`msg ${cls}`);
+    }
+
+    async submit(e) {
+        e.preventDefault();
+        const $word = $(".word", this.board)
+
+        let word = $word.val()
+        let len = word.length
+        if (len < 2) {
+            this.showMsg(`Word has to be at least 2 letters long`);
+            return;
+        }
+
+        if (!word) return;
+
+        if (this.words.has(word)) {
+            this.showMsg(`Already found ${word}`);
+            return;
+        }
+
+        const resp = await axios.get("/check-word", { params: { word: word } });
+        if (resp.data.result === "not-word") {
+            this.showMsg(`${word} is not a valid English word`, "err");
+        } else if (resp.data.result === "not-on-board") {
+            this.showMsg(`${word} is not a valid word on this board`, "err");
+        } else {
+            this.showWord(word);
+            this.score += word.length;
+            this.showScore();
+            this.words.add(word);
+            this.showMsg(`Word added: ${word}`, "ok");
+        }
+        $word.val("").focus();
+
+    }
+
     showScore() {
-      $(".score", this.board).text(this.score);
+        $(".score", this.board).text(this.score);
     }
-  
-    /* show a status message */
-  
-    showMessage(msg, cls) {
-      $(".msg", this.board)
-        .text(msg)
-        .removeClass()
-        .addClass(`msg ${cls}`);
-    }
-  
-    /* handle submission of word: if unique and valid, score & show */
-  
-    async handleSubmit(evt) {
-      evt.preventDefault();
-      const $word = $(".word", this.board);
-  
-      let word = $word.val();
-      if (!word) return;
-  
-      if (this.words.has(word)) {
-        this.showMessage(`Already found ${word}`, "err");
-        return;
-      }
-  
-      // check server for validity
-      const resp = await axios.get("/check-word", { params: { word: word }});
-      if (resp.data.result === "not-word") {
-        this.showMessage(`${word} is not a valid English word`, "err");
-      } else if (resp.data.result === "not-on-board") {
-        this.showMessage(`${word} is not a valid word on this board`, "err");
-      } else {
-        this.showWord(word);
-        this.score += word.length;
-        this.showScore();
-        this.words.add(word);
-        this.showMessage(`Added: ${word}`, "ok");
-      }
-  
-      $word.val("").focus();
-    }
-  
-    /* Update timer in DOM */
-  
+
     showTimer() {
-      $(".timer", this.board).text(this.secs);
+        $(".timer", this.board).text(this.secs);
     }
-  
-    /* Tick: handle a second passing in game */
-  
-    async tick() {
-      this.secs -= 1;
-      this.showTimer();
-  
-      if (this.secs === 0) {
-        clearInterval(this.timer);
-        await this.scoreGame();
-      }
+
+    async clock() {
+        this.secs -= 1;
+        this.showTimer();
+
+        if (this.secs === 10) {
+            $('.timer').css("color", "red")
+        }
+        if (this.secs === 0) {
+            clearInterval(this.timer);
+            await this.gameEnds();
+        }
     }
-  
-    /* end of game: score and update message. */
-  
-    async scoreGame() {
-      $(".add-word", this.board).hide();
-      const resp = await axios.post("/end-game", { score: this.score });
-      if (resp.data) {
-        this.showMessage(`New score: ${this.score}`, "ok");
-      }
+
+    async gameEnds() {
+        $(".add-word", this.board).hide();
+        const res = await axios.post("/end-game", { score: this.score });
+        if (res) {
+            this.showMsg(`Final score: ${this.score}`, "ok");
+        }
+        return;
     }
-  }
+}
+
--- test.py
+++ test.py
@@ -6,5 +6,23 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Stuff to do before every test."""
 
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Make sure information is in the session and HTML is displayed"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertIn('board', session)
+            self.assertIn(b'Score:', response.data)
+
+    def test_invalid_word(self):
+        """Test if word is in the list of words"""
+
+        self.client.get('/')
+        response = self.client.get('/check-word?word=impossible')
+        self.assertEqual(response.json['result'], 'not-on-board')
